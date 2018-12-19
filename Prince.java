4a26655b9cbc7fcd5bb539dec3b32643fb214f34
larsvancutsem <lars.vancutsem@hotmail.com>
2018-12-19 22:25:18 +0100

    Delete Prince.java

--- Prince.java
+++
@@ -1,9 +0,0 @@
-package Game;
-
-public class Prince extends Unit {
-
-	public Prince(String name, int maxhp, int hit, int atk, int def) {
-		//prince can only move two tiles per turn
-		super(name,maxhp,hit,atk,def,2);
-	}
-}
