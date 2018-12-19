47434f58489974138f31ef3d6cb87a9c730c5e32
larsvancutsem <lars.vancutsem@hotmail.com>
2018-12-19 22:26:10 +0100

    Delete Boss.java

--- Boss.java
+++
@@ -1,9 +0,0 @@
-package Game;
-
-public class Boss extends Unit {
-
-	public Boss(String name, int maxhp, int hit, int atk, int def) {
-		//Boss can not move in any turn (he only can attack)
-		super(name,maxhp,hit,atk,def,0);
-	}
-}
